d9df0a4a17b1b15185425494f6f583fbdfbf0b99
yfktn <friskantoni@gmail.com>
2020-03-03 16:39:48 +0700

    add the permission setting

--- controllers/MultiCaptcha.php
+++ controllers/MultiCaptcha.php
@@ -8,12 +8,13 @@ class MultiCaptcha extends Controller
 {
 	public $implement = ['Backend.Behaviors.FormController'];
     public $formConfig = 'config_form.yaml';
-	
+	public $requiredPermissions = [
+		'vijaywilson.multicaptcha.set_configuration'
+	];
+
 	public function __construct()
 	{
 		parent::__construct();
 		BackendMenu::setContext('VijayWilson.Multicaptcha','multicaptcha','multicaptcha');
 	}
 }
-
-?>
